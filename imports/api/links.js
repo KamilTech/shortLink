e8944c33935cb565711078e3422a19de2269f5e2
Kamil <kamil.mich50@gmail.com>
2018-08-18 22:33:21 +0200

    Link Analytics

--- imports/api/links.js
+++ imports/api/links.js
@@ -55,5 +55,22 @@ Meteor.methods({
                 visible
             }
         });
+    },
+    'links.trackVisit'(_id) {
+        new SimpleSchema({
+            _id: {
+            type: String,
+            min: 1
+            }
+        }).validate({ _id });
+
+        Links.update({ _id }, {
+            $set: {
+                lastVisitedAt: new Date().getTime()
+            },
+            $inc: {
+                visitedCount: 1
+            }
+        });
     }
 });
